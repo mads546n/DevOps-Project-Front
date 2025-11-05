1678dcc1ff4d4bfb84220ba4377011d718593897
JalokinBo <144294697+JalokinBo@users.noreply.github.com>
2025-11-05 11:00:58 +0100

    Added scripts and dependencies. Updated vite.config.js for testing. Setup first test.

---
+++ tests/setup.js
@@ -0,0 +1,16 @@
+import {useState} from 'react';
+
+//just an example at the moment - this will not be used
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </div>
+  );
+}
+
+export default Counter;
